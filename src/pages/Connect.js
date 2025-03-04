c921548e2a263dd82ed6dd596395dcfe5b7cf340
Deepak8642 <deepakdavala77@gmail.com>
2025-03-05 02:20:34 +0530

    react projects

---
+++ src/pages/Connect.js
@@ -0,0 +1,149 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { Switch } from '@headlessui/react';
+
+// Icons
+import { RiInstagramFill, RiFacebookFill, RiTwitterFill, RiYoutubeFill } from 'react-icons/ri';
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+}
+
+const Connect = () => {
+    const [agreed, setAgreed] = useState(false);
+    const [submitted, setSubmitted] = useState(false); // New state to track submission
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Simulating form submission
+        setSubmitted(true);
+    };
+
+    return (
+        <div className="isolate mt-44 mb-36 p-6 mx-auto max-w-sm sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
+            <div
+                className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+                aria-hidden="true"
+            >
+                <div
+                    className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                />
+            </div>
+
+            <div className="mx-auto text-center">
+                <h2 className="title">{submitted ? "Thank You!" : "Contact Us"}</h2>
+                <p className="mt-2 subdesc">
+                    {submitted
+                        ? "Your message has been successfully sent. We will get back to you soon."
+                        : "We are here to assist you on your study abroad journey."}
+                </p>
+            </div>
+
+            {submitted ? (
+                <div className="text-center mt-10">
+                    <p className="text-green-600 text-lg font-semibold">Form submitted successfully!</p>
+                </div>
+            ) : (
+                <form onSubmit={handleSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20">
+                    <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                        <div>
+                            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-600">
+                                First name
+                            </label>
+                            <div className="mt-2.5">
+                                <input
+                                    type="text"
+                                    name="first-name"
+                                    id="first-name"
+                                    autoComplete="given-name"
+                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-600">
+                                Last name
+                            </label>
+                            <div className="mt-2.5">
+                                <input
+                                    type="text"
+                                    name="last-name"
+                                    id="last-name"
+                                    autoComplete="family-name"
+                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-600">
+                                Email
+                            </label>
+                            <div className="mt-2.5">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    autoComplete="email"
+                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-600">
+                                Message
+                            </label>
+                            <div className="mt-2.5">
+                                <textarea
+                                    name="message"
+                                    id="message"
+                                    rows={4}
+                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    defaultValue={''}
+                                />
+                            </div>
+                        </div>
+                        <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2 justify-center">
+                            <div className="flex h-6 items-center">
+                                <Switch
+                                    checked={agreed}
+                                    onChange={setAgreed}
+                                    className={classNames(
+                                        agreed ? 'bg-primary' : 'bg-gray-200',
+                                        'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                                    )}
+                                >
+                                    <span className="sr-only">Agree to policies</span>
+                                    <span
+                                        aria-hidden="true"
+                                        className={classNames(
+                                            agreed ? 'translate-x-3.5' : 'translate-x-0',
+                                            'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
+                                        )}
+                                    />
+                                </Switch>
+                            </div>
+                            <Switch.Label className="text-sm leading-6 text-gray-700">
+                                By selecting this, you agree to allow us to contact you via phone
+                            </Switch.Label>
+                        </Switch.Group>
+                    </div>
+                    <div className="mt-10">
+                        <button
+                            type="submit"
+                            className="block w-full rounded-xl bg-primary px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xl hover:bg-primary-hover focus:bg-primary-hover duration-300 transition-colors"
+                        >
+                            Let's talk
+                        </button>
+                    </div>
+                </form>
+            )}
+        </div>
+    );
+};
+
+export default Connect;
